ee4f4901c74ec123be52bed5903c1a0bc00d4394
Damian Cybiak <d.cybiak@softwizard.pl>
2020-05-08 23:38:54 +0200

    better format code (for me) in debug.php. Include debug in index

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php 
 
+require_once ("src/Utils/debug.php");
+
 $test = "test";
-
-function dump($test)
-{
-  echo '<div 
-    style = "display: inline-block;
-    padding: 0 10px;
-    border: 1px solid gray;
-    background-color: lightgray;
-  "
-  >
-  <pre>';
-  print_r($test);
-  echo '<pre>
-  </div>';
-}
-
 dump($test);
--- src/Utils/Debug.php
+++ src/Utils/Debug.php
@@ -2,15 +2,12 @@
 
 function dump($test)
 {
-  echo '<div 
-    style = "display: inline-block;
-    padding: 0 10px;
-    border: 1px solid gray;
-    background-color: lightgray;
-  "
-  >
-  <pre>';
-  print_r($test);
-  echo '<pre>
-  </div>';
+  echo 
+    '<div style = "display: inline-block;
+      padding: 0 10px;
+      border: 1px solid gray;
+      background-color: lightgray;">
+
+      <pre>';print_r($test);'</pre>
+    </div>';
 }
