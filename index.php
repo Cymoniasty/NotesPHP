5bee3035bf1342daf20d18beecee9377106cc6b0
Damian Cybiak <d.cybiak@softwizard.pl>
2020-05-09 00:16:47 +0200

    First version of structure html

--- index.php
+++ index.php
@@ -9,3 +9,33 @@ require_once ("src/Utils/debug.php");
 $test = "test";
 $secondaryTest = $test;
 dump($secondaryTest);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NotatnikPHP</title>
+</head>
+<body>
+  
+  <div class="header">
+    <h1>Moje notatki</h1>
+  </div>
+
+  <div class="container">
+    <div class="menu">
+      <ul>
+        <li><a href="/">Lista notatek</a></li>
+        <li><a href="/?action=create">Nowa notatka</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="text">Treść</div>
+  <div class="footer"></div>
+
+</body>
+</html>
