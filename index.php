38a66034fef33f76a8f74f399c08d8b92750d1ab
Damian Cybiak <d.cybiak@softwizard.pl>
2020-05-09 01:16:24 +0200

    separate logic of view

--- index.php
+++ index.php
@@ -9,40 +9,10 @@ require_once ("src/Utils/debug.php");
 
 $action = $_GET['action'] ?? null;
 
-?>  
+if ($action === 'create'){
+  include_once("templates/pages/create.php");
 
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>NotatnikPHP</title>
-</head>
-<body>
-  
-  <div class="header">
-    <h1>Moje notatki</h1>
-  </div>
+} else {
+  include_once("templates/pages/list.php");
 
-  <div class="container">
-    <div class="menu">
-      <ul>
-        <li><a href="/">Lista notatek</a></li>
-        <li><a href="/?action=create">Nowa notatka</a></li>
-      </ul>
-    </div>
-  </div>
-
-  <div>
-    <?php if ($action === 'create'): ?>
-      <h3>Nowa notatka</h3>
-      <?php echo htmlentities($action) ?>
-    <?php else: ?>
-      <h4>Lista notatek</h4>
-      <?php echo htmlentities($action ?? '') ?>
-    <?php endif; ?>
-  </div>
-
-  <div class="footer"></div>
-</body>
-</html>
+};
