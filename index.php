b36ea117ff83d1c126106a4de2c600b5bc7ff809
Damian Cybiak <d.cybiak@softwizard.pl>
2020-05-12 22:54:23 +0200

    Add some comments. New site look.

--- index.php
+++ index.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 
 namespace App;
 
+// Zakomentować ten plik debug.php jak pójdzie już projekt na proda
 require_once("src/Utils/debug.php");
 require_once("src/view.php");
+
+// Jak już projekt poleci na produkcję to wyłącz pokazywanie błędów:
+// error_reporting(0);
+// ini_set("display_errors", "0");
 
 const DEFAULT_ACTION = "list";
 
 $action = $_GET['action'] ?? DEFAULT_ACTION;
 
+$view = new View();
 
 $viewParams = [];
 if ($action === 'create') {
+    $page = 'create';
     $viewParams['resultCreate'] = "Udało się!";
 } else {
+    $page = 'list';
     $viewParams['resultList'] = "Wyświetlamy notatki";
 };
 
-$view = new View();
-$view->render($action, $viewParams);
+$view->render($page, $viewParams);
--- templates/layout.php
+++ templates/layout.php
@@ -5,29 +5,36 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NotatnikPHP</title>
+  <script src="https://kit.fontawesome.com/891c3de228.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="/public/style.css">
 </head>
 
-<body>
+<body class="body">
+  <div class="wrapper">
 
-  <!-- Główny widok -->
-  <div class="header">
-    <h1>Moje notatki</h1>
-  </div>
-  <div class="container">
-    <div class="menu">
-      <ul>
-        <li><a href="/">Lista notatek</a></li>
-        <li><a href="/?action=create">Nowa notatka</a></li>
-      </ul>
+    <!-- Główny widok -->
+    <div class="header">
+      <h1><i class="far fa-clipboard"></i>Moje notatki</h1>
+    </div>
+
+    <div class="container">
+      <div class="menu">
+        <ul>
+          <li><a href="/">Lista notatek</a></li>
+          <li><a href="/?action=create">Nowa notatka</a></li>
+        </ul>
+      </div>
+
+      <!-- Renderowanie odpowiedniego widoku -->
+      <div class="page">
+        <?php require_once("templates/pages/$page.php"); ?>
+      </div>
+    </div>
+
+    <div class="footer">
+      <p>Notatki - projekt w kursie PHP</p>
     </div>
   </div>
-
-  <!-- Renderowanie odpowiedniego widoku -->
-  <?php
-  require_once("templates/pages/$page.php");
-  ?>
-
-  <div class="footer">Stopka</div>
 </body>
 
 </html>
