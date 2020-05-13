a80446956b1e905166acef32cf3bf47b0409782b
Damian Cybiak <d.cybiak@softwizard.pl>
2020-05-13 21:28:45 +0200

    small changes in debug.php for better view

--- src/Utils/Debug.php
+++ src/Utils/Debug.php
@@ -3,19 +3,21 @@
 declare(strict_types=1);
 
 error_reporting(E_ALL);
-ini_set("display_errors", "1");
+ini_set('display_errors', '1');
 
 function dump($test)
-{ 
+{
   echo '<br/><div 
-    style = "display: inline-block;
+    style="
+    display: inline-block;
     padding: 0 10px;
     border: 1px solid gray;
     background-color: lightgray;
-  "
+    "
   >
   <pre>';
   print_r($test);
   echo '</pre>
-  <br/</div>';
+  </div>
+  <br/>';
 }
