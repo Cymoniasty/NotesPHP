afc872dd05985ef9d4944bcccb72e28e1aeecde0
Damian Cybiak <d.cybiak@softwizard.pl>
2020-06-03 22:50:31 +0200

    still refactor

--- src/Controller.php
+++ src/Controller.php
@@ -27,7 +27,7 @@ class Controller
     self::$configuration = $configuration;
   }
 
-  public function __construct(array $request)
+  public function __construct(Request $request)
   {
     if (empty(self::$configuration['db'])) {
       throw new ConfigurationException("Configuration error");
@@ -38,55 +38,71 @@ class Controller
     $this->view = new View();
   }
 
+
+  public function createAction()
+  {
+
+    if ($this->request->hasPost()) {
+      $noteData = [
+        'title' => $this->request->postParam('title'),
+        'description' => $this->request->postParam('description')
+      ];
+      $this->database->createNote($noteData); // tworzymy tutaj notatkę i przekazujemy wymagane dane
+      header('Location:/?before=created'); // po utworzeniu notatki przenosi nas do strony głównej
+      exit;
+    }
+
+    $this->view->render('create');
+  }
+
+  public function showAction()
+  {
+
+    $noteId = (int) $this->request->getParam('id');
+
+    if (!$noteId) {
+      header('Location:/?error=missingNoteId');
+      exit;
+    }
+
+    try {
+      $note = $this->database->getNote($noteId);
+    } catch (NotFoundException $e) {
+      header('Location:/?error=noteNotFound');
+      exit;
+    }
+
+    $viewParams = [
+      'note' => $note
+    ];
+
+    $this->view->render(
+      'show',
+      ['note' => $note]
+    );
+  }
+
+  public function listAction()
+  {
+
+    $this->view->render(
+      'list',
+      [
+        'notes' => $this->database->getNotes(),
+        'before' => $this->request->getParam('before'),
+        'error' => $this->request->getParam('error')
+      ]
+    );
+  }
+
   public function run(): void
   {
-    switch ($this->action()) {
-      case 'create':
-        $page = 'create';
-
-
-        if ($this->request->hasPost()) {
-          $noteData = [
-            'title' => $this->request->postParam('title'),
-            'description' => $this->request->postParam('description')
-          ];
-          $this->database->createNote($noteData); // tworzymy tutaj notatkę i przekazujemy wymagane dane
-          header('Location:/?before=created'); // po utworzeniu notatki przenosi nas do strony głównej
-          exit;
-        }
-        break;
-      case 'show':
-        $page = 'show';
-
-        $noteId = (int) $this->request->getParam('id');
-
-        if (!$noteId) {
-          header('Location:/?error=missingNoteId');
-          exit;
-        }
-
-        try {
-          $note = $this->database->getNote($noteId);
-        } catch (NotFoundException $e) {
-          header('Location:/?error=noteNotFound');
-          exit;
-        }
-
-        $viewParams = [
-          'note' => $note
-        ];
-        break;
-      default:
-        $page = 'list';
-
-        $viewParams = [
-          'notes' => $this->database->getNotes(),
-          'before' => $this->request->getParam('before'),
-          'error' => $this->request->getParam('error')
-        ];
-        break;
+    $action = $this->action() . 'Action';
+    if (!method_exists($this, $action)) {
+      $action = self::DEFAULT_ACTION . 'Action';
     }
-    $this->view->render($page, $viewParams ?? []);
+
+    $this->$action;
   }
 
   private function action(): string
